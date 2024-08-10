5c626927bb799efd6585419063d4fd7308244d40
zeinab montaghemi <zeinab.montaghemi@gmail.com>
2024-08-10 19:47:04 +0330

    update component add new files

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,11 +1,9 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
-import Logo from "./Logo";
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
-      <Logo />
       <ul>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
