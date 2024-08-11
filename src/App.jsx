98c39830caffbe5e300e65b3b5b311b3458b4d56
zeinab montaghemi <zeinab.montaghemi@gmail.com>
2024-08-11 12:37:30 +0330

    impelementing citites list

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,30 @@ import Pricing from "./pages/Pricing";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:9000";
 
 function App() {
-  const [cities, setCities] = useState({});
-  const [isLoading, setIsLoading] = setState(false);
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
 
   return (
     <BrowserRouter>
@@ -20,8 +39,14 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList />} />
-          <Route path="cities" element={<CityList />} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,22 @@
+import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
 
-function CityList() {
-  return <ul className={styles.CityList}></ul>;
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.lenght)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+  return (
+    <ul className={styles.CityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default CityList;
