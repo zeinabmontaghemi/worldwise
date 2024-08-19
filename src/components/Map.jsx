7ff31c02ec81b30bb866fc12d61b39a77f9053d5
zeinab montaghemi <zeinab.montaghemi@gmail.com>
2024-08-19 15:17:28 +0330

    correct lng state in url and solve promlem of jumping of map when click back button

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,21 +1,31 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../context/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
   const { cities } = useCities();
-  const [mapPosition, setMapPosition] = useState([40, 20]);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const [searchParams] = useSearchParams();
+
+  const maplat = searchParams.get("lat");
+  const maplng = searchParams.get("lng");
+
+  useEffect(
+    function () {
+      if (maplat && maplng) setMapPosition([maplat, maplng]);
+    },
+    [maplat, maplng]
+  );
+
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition}
-        zoom={13}
+        // center={[maplat, maplng]}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -33,9 +43,17 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+
+  return null;
 }
 
 export default Map;
