f4e9298ea9887146afaec373e494bb8c669d437f
zeinab montaghemi <zeinab.montaghemi@gmail.com>
2024-08-26 19:44:28 +0330

    handel login and logout

--- src/context/FakeAuthContext.jsx
+++ src/context/FakeAuthContext.jsx
@@ -50,6 +50,8 @@ function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error("AuthContext was used outside AuthProvider");
+
+  return context;
 }
 
 export { AuthProvider, useAuth };
