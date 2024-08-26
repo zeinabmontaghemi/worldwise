e7032ee5ece599c2702db1d7e8bba63f3dddab9f
zeinab montaghemi <zeinab.montaghemi@gmail.com>
2024-08-26 21:33:19 +0330

    Adding Fake Authentication Protecting a Route and changing app logo and name in browser tab

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -3,6 +3,7 @@ import Sidebar from "../components/Sidebar";
 import User from "../components/User";
 
 import styles from "./AppLayout.module.css";
+import ProtectedRoute from "./ProtectedRoute";
 
 function AppLayout() {
   return (
