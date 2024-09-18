35194b8cde77ef0af7419fb895798fb183775f4e
zeinab montaghemi <zeinab.montaghemi@gmail.com>
2024-09-18 21:17:04 +0330

    change publish = build in toml file

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  build: {
+    outDir: "build",
+  },
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:9000",
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ""),
-  //     },
-  //   },
-  // },
+
   base: "/",
 });
