626380b9176ee83bd4699c4352f6e44ab0623f93
zeinab montaghemi <zeinab.montaghemi@gmail.com>
2024-09-20 15:10:09 +0330

    change url

--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -6,8 +6,10 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:9000";
+// const BASE_URL = "http://localhost:9000";
 // const BASE_URL = "https://worldwize-zeinab.netlify.app/.netlify/functions";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:9000";
 
 const CitiesContext = createContext();
 
